26676389b1b09f261a2bc4d395a5a643e815cbf9
Asus <kidepdone@gmail.com>
2024-02-01 01:15:04 +0330

    model Clinic

--- piska_django/models.py
+++ piska_django/models.py
@@ -4,3 +4,8 @@ class Patient(models.Model):
     username = models.CharField(max_length=300)
     password = models.IntegerField()
     email = models.EmailField(max_length=264)
+
+class Clinic(models.Model):
+    name = models.CharField(max_length=300)
+    capacity =  models.IntegerField()
+
