cf5d2650a14491df9bbaab43bc06136253567fee
Asus <kidepdone@gmail.com>
2024-02-01 01:21:26 +0330

    model Staff

--- piska_django/models.py
+++ piska_django/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
+
 class Patient(models.Model):
     username = models.CharField(max_length=300)
     password = models.IntegerField()
     email = models.EmailField(max_length=264)
 
+
 class Clinic(models.Model):
     name = models.CharField(max_length=300)
-    capacity =  models.IntegerField()
+    capacity = models.IntegerField()
 
+
+class Staff(models.Model):
+    username = models.CharField(max_length=300)
+    password = models.IntegerField()
+    email = models.EmailField(max_length=264)
+    clinic = models.ForeignKey(Clinic, on_delete=models.CASCADE)
